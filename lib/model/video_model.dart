efbebfc6e4f73a9d0a1379095d4d72f9f0ebac45
Morvalbrock <agilansr222@gmail.com>
2024-04-17 09:16:12 +0530

    static content language change complete

---
+++ lib/model/video_model.dart
@@ -0,0 +1,10 @@
+class VideoItem {
+  final String videoUrl;
+  final String title;
+  final String thumbnailUrl;
+
+  VideoItem(
+      {required this.videoUrl,
+      required this.title,
+      required this.thumbnailUrl});
+}
